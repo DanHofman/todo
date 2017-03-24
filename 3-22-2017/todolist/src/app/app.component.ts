a7b8576f520b5f632b344dd8fddc178980c7e541
Hofman <dhofman@pattersoncompanies.com>
2017-03-24 14:26:36 -0700

    added features

--- 3-22-2017/todolist/src/app/app.component.ts
+++ 3-22-2017/todolist/src/app/app.component.ts
@@ -11,7 +11,7 @@ import {FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   formed;
-  importance = ["high", "medium", "meh"];
+  importance = ["high", "medium", "meh", "non"];
 
   todo: Checklistitem[];
   constructor(private todoservice: TodoService) {
@@ -24,18 +24,26 @@ export class AppComponent implements OnInit {
       time: new FormControl('', Validators.required),
       importance: new FormControl('', Validators.required)
     });
-    // this.todoservice.addCheckListItem(new Checklistitem("this is very important", "now", "high", 0))
-    // this.todoservice.addCheckListItem(new Checklistitem("this is somewhat important", "soon", "medium", 0))
-    // this.todoservice.addCheckListItem(new Checklistitem("this isn't so important", "I dunno", "meh", 0))
+
+    this.todoservice.addCheckListItem(new Checklistitem("this is somewhat important", "soon", "medium", 0))
+    this.todoservice.addCheckListItem(new Checklistitem("this is very important", "now", "high", 0))
+    this.todoservice.addCheckListItem(new Checklistitem("this is somewhat important", "soon", "medium", 0))
+    this.todoservice.addCheckListItem(new Checklistitem("this is very important", "now", "high", 0))
+    this.todo = this.todoservice.getAllItems();
   }
 
   onDelete(id){
     this.todoservice.removeCheckListItem(id);
+    this.todo = this.todoservice.getAllItems();
   }
-
+  onCompleted(id){
+    this.todoservice.completeCheckListItem(id);
+    this.todo = this.todoservice.getAllItems();
+  }
   onSubmit(newtodo){
    let newChecklistItem: Checklistitem = new Checklistitem(newtodo.name, newtodo.time, newtodo.importance, this.todoservice.getLength());
    this.todoservice.addCheckListItem(newChecklistItem);
+   this.todo = this.todoservice.getAllItems();
    this.formed.reset();
   }
 }
--- 3-22-2017/todolist/src/app/todo.service.ts
+++ 3-22-2017/todolist/src/app/todo.service.ts
@@ -3,23 +3,69 @@ import { Checklistitem } from './checklistitem'
 export class TodoService {
   private todos: Checklistitem[] = [];
 
+  getChecklistItem(id: number){
+    return this.todos[id];
+  }
   addCheckListItem(item: Checklistitem) {
     this.todos.push(item);
-    this.reNumberTodoList();
+    this.reOrderTodosAfterSort();
   }
   removeCheckListItem(id: number){
     this.todos.splice(id, 1);
-    this.reNumberTodoList();
+    this.reOrderTodosAfterSort();
+  }
+  completeCheckListItem(id: number) {
+    this.todos[id].importance = "non";
+    this.reOrderTodosAfterSort();
   }
   getAllItems() {
     return this.todos;
   }
-  reNumberTodoList() {
-    for(var i = 0; i < this.todos.length; i++) {
+  reOrderTodosAfterSort(){
+    this.todos = this.sortTodo(this.todos);
+    console.log(this.todos);
+    console.log("during reorder");
+    for(var i = 0; i < this.todos.length; i ++){
       this.todos[i].id = i;
     }
   }
+  sortTodo(todos) {
+    if(todos.length < 2){
+      return todos;
+    }
+    var middle = todos.length / 2;
+    var left = todos.slice(0, middle);
+    var right = todos.slice(middle, todos.length);
+
+    return this.mergeTodoafterSorting(this.sortTodo(left), this.sortTodo(right));
+  }
+  mergeTodoafterSorting(lefttodo, righttodo){
+    var resulting = [];
+
+    while (lefttodo.length && righttodo.length){
+      if(lefttodo[0].importance == "non" ){
+        resulting.push(righttodo.shift());    
+      }else if(righttodo[0].importance == "non" ){
+        resulting.push(lefttodo.shift());
+      }else if (lefttodo[0].importance == "high"){
+        resulting.push(lefttodo.shift());
+      } else if (righttodo[0].importance == "high") {
+        resulting.push(righttodo.shift());
+      } else if (lefttodo[0].importance == "medium" && righttodo[0].importance != "high" && righttodo[0].importance != "medium"){
+        resulting.push(lefttodo.shift());
+      } else if (righttodo[0].importance == "medium" && lefttodo[0].importance != "high" && lefttodo[0].importance != "medium"){
+        resulting.push(righttodo.shift());
+      } else {
+        resulting.push(lefttodo.shift());
+      }
+      }while (lefttodo.length){
+        resulting.push(lefttodo.shift());
+      }while (righttodo.length){
+        resulting.push(righttodo.shift());
+      }
+      return resulting;   
+  }
   getLength() {
     return this.todos.length;
+    }
   }
-}
